a4e078bb90d68bc95ceedb035aca74fd438025da
hsycamp <hsycamp@gmail.com>
2019-10-17 11:11:07 +0900

    refactor : google 로그인을 위한 db 스키마 수정
    document에 googleId추가
    google Oauth로 회원가입시 비밀번호 암호화 제외

--- models/user.js
+++ models/user.js
@@ -3,17 +3,23 @@ const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   id: String,
-  password: String
+  password: String,
+  auth: {
+    googleId: String
+  }
 });
 
 userSchema.pre("save", async function(next) {
+  if (this.auth.googleId) {
+    next();
+  }
   const hashedPassword = await bcrypt.hash(this.password, 10);
   this.password = hashedPassword;
   next();
 });
 
 userSchema.methods.checkPassword = async function(inputPassword) {
-  const checkResult = await bcrypt.compare(inputPassword,this.password);
+  const checkResult = await bcrypt.compare(inputPassword, this.password);
   return checkResult;
 };
 
