9e1dd746939ac70f9ac5fa1024d26a83e556bef6
hsycamp <hsycamp@gmail.com>
2019-10-17 11:20:35 +0900

    feat : google 로그인, google 로그인 콜백 라우터 설정

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,10 @@ const authController = require("../controller/auth-controller");
 /* local signIn. */
 router.post("/local", authController.signIn);
 
+router.get("/google", authController.googleSignIn);
+
+router.get("/google/callback", authController.googleSignInCallback);
+
 router.post("/sign-out", authController.signOut);
 
 module.exports = router;
