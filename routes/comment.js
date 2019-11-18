2b6e6dac1c4cfadd8beb59a47c381a501d3905fc
hsycamp <hsycamp@gmail.com>
2019-11-18 18:50:52 +0900

    feat : 댓글 삭제 라우터 추가

--- routes/comment.js
+++ routes/comment.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const commentController = require("../controller/comment-controller");
 
 router.post("/", commentController.addComment);
+router.delete("/:commentId", commentController.deleteComment);
 
 module.exports = router;
